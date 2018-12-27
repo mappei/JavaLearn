a4b97203ed79d373760195b031c0dbdf30662729
libra <2990581485@qq.com>
2018-12-27 22:06:15 +0800

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/list/ArrayList.java
@@ -0,0 +1,31 @@
+package list;
+
+import java.util.Collection;
+
+public class ArrayList {
+
+    private Object[] elementData;
+    private int index;
+
+    public static final Object[] DEFAULT_VALUE = {};
+    public static final Object[] DEFAULT = {};
+
+    //
+    public ArrayList(){
+        elementData = DEFAULT_VALUE;
+    }
+
+    public ArrayList(int index){
+        if(){
+
+        }
+    }
+
+    public ArrayList(Collection collection){
+
+    }
+    public void add(){
+
+    }
+
+}
