46ef2f7ce5d7999abeaedbc562e952d2d86759b9
libra <2990581485@qq.com>
2018-12-27 23:30:27 +0800

    idea

--- src/list/ArrayList.java
+++ src/list/ArrayList.java
@@ -1,31 +1,58 @@
 package list;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class ArrayList {
 
     private Object[] elementData;
-    private int index;
+    private int size;
+    private int modCount;
 
-    public static final Object[] DEFAULT_VALUE = {};
-    public static final Object[] DEFAULT = {};
+    public static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    public static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final int DEFAULT_CAPACITY = 10;
 
     //
-    public ArrayList(){
-        elementData = DEFAULT_VALUE;
+    public ArrayList() { this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
     }
 
-    public ArrayList(int index){
-        if(){
-
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("illegal initialCapacity: " + initialCapacity);
+        } else if (initialCapacity == 0){
+            this.elementData = EMPTY_ELEMENTDATA;
         }
+        this.elementData = new Object[initialCapacity];
     }
 
     public ArrayList(Collection collection){
+        this.elementData = collection.toArray();
+        if(elementData.length == 0){
+            this.elementData = EMPTY_ELEMENTDATA;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(elementData);
+    }
+
+    public boolean add(Object obj) {
+
+
+        return true;
+    }
+
+    public void add(int index, Object obj){
 
     }
-    public void add(){
 
+    public int indexOf(Object obj){
+        for(int i=0; i<){
+
+        }
+        return
     }
 
 }
---
+++ src/list/ArrayListTest.java
@@ -0,0 +1,11 @@
+package list;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args){
+        ArrayList list = new ArrayList(3);
+        list.indexOf()
+    }
+
+}
