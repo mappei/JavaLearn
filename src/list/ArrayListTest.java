9d2e6edd71957713511d8667fe36b66ff4edb579
libra.ma <libra.ma@backstopdevelopment.com>
2018-12-28 17:54:30 +0800

    Compare with ArrayList & LinkedList for Add

--- src/list/ArrayListTest.java
+++ src/list/ArrayListTest.java
@@ -1,17 +1,20 @@
 package list;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class ArrayListTest {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         /*ArrayList list = new ArrayList();
         //ArrayList list = new ArrayList();
         list.add("hbjnkml,;n");
         list.remove(-32);*/
 
         //remove
-        ArrayList list = new ArrayList();
+       /* ArrayList list = new ArrayList();
         list.add(12);
         list.add(1111);
         list.add(343);
@@ -31,7 +34,32 @@ public class ArrayListTest {
 
         //System.out.println(afdsa.toString());
         System.out.println(list.toString());
-        System.out.println(list.size());
+        System.out.println(list.size());*/
+
+
+
+        LinkedList<Integer> link = new LinkedList<>();
+        for (int i = 1; i < 10000000; i++) {
+            link.add(i);
+        }
+        long linkBefore = System.currentTimeMillis();
+        for (int i = 1; i < 1000; i++) {
+            link.add(5002,1);
+        }
+        long linkAfter = System.currentTimeMillis();
+        System.out.println("link:" + (linkAfter - linkBefore));
+
+
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 1; i < 10000000; i++) {
+            array.add(i);
+        }
+        long arrayBefore = System.currentTimeMillis();
+        for (int i = 1; i < 1000; i++) {
+            array.add(5002,1);
+        }
+        long arrayAfter = System.currentTimeMillis();
+        System.out.println("array:" + (arrayAfter - arrayBefore));
     }
 
 }
